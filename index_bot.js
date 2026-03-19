9c8e2c614cbd3cd9acfd7e612adb0e485718d6ae
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2026-03-19 01:41:38 -0800

    Update index_bot.js

--- index_bot.js
+++ index_bot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const TelegramBot = require('alfatehisp_boot_telegram');
+const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
@@ -8,13 +8,26 @@ const API_URL = process.env.API_URL || 'https://api.alfateh.cloudtech-it.com';
 const PORT = process.env.PORT || 5000;
 
 const bot = new TelegramBot(TOKEN, { polling: false });
-
+const webhookUrl = `https://alfatehisp-boot-telegram.onrender.com/webhook/${TOKEN}`;
 const app = express();
 app.use(bodyParser.json());
 
 app.post(`/webhook/${TOKEN}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
+});
+app.listen(PORT, async () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log("Webhook URL:", webhookUrl);
+
+  try {
+    await bot.deleteWebHook(); // مهم
+    await bot.setWebHook(webhookUrl);
+
+    console.log("✅ Webhook set successfully");
+  } catch (err) {
+    console.error("❌ Webhook error:", err.message);
+  }
 });
 
 // 🔥 جلسات المستخدمين
